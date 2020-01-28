73dc0ef27a4ab91ee7a21fe5ae51de95bc44432e
Zeming Lin <ebetica0@gmail.com>
2020-01-28 13:58:31 -0500

    More configs

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ cp -f ~/.zshrc ~/.zshrc.backup
 cp -f ~/.tmux.conf ~/.tmux.conf.backup
 cp -f ~/.gitconfig ~/.gitconfig.backup
 cp -f ~/.gitignore ~/.gitignore.backup
+cp -f ~/.i3/config ~/.i3/config.backup
 
 mkdir -p ~/.config/kak
 cp -f ~/.config/kak/kakrc ~/.config/kak/kakrc.backup
@@ -15,12 +16,13 @@ mkdir -p ~/.oh-my-zsh/custom/themes
 mkdir -p ~/.oh-my-zsh/custom/plugins
 (cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting)
 
-ln -s ~/dotfiles/.vimrc ~/
-ln -s ~/dotfiles/.tmux.conf ~/
-ln -s ~/dotfiles/gitignore ~/.gitignore
-ln -s ~/dotfiles/kakrc ~/.config/kak/kakrc
+ln -sf ~/dotfiles/.vimrc ~/
+ln -sf ~/dotfiles/.tmux.conf ~/
+ln -sf ~/dotfiles/gitignore ~/.gitignore
+ln -sf ~/dotfiles/kakrc ~/.config/kak/kakrc
+ln -sf ~/dotfiles/i3config ~/.i3/config
 
-ln -s ~/dotfiles/ebetica.zsh-theme ~/.oh-my-zsh/custom/themes/
+ln -sf ~/dotfiles/ebetica.zsh-theme ~/.oh-my-zsh/custom/themes/
 echo "source /etc/profile" >> ~/.zshrc
 echo "source ~/dotfiles/.zshrc" >> ~/.zshrc
 
